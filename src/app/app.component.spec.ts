c2f424365a7d88d028ab4135d9d52f9754db2902
sakthiveldhanasekaran <sakthiveldhanasekaran@live.com>
2023-09-27 22:43:37 +0530

    Turned off eslint quotes rule

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,7 +16,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should have as title \'portfolio\'', () => {
+  it(`should have as title 'portfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('portfolio');
